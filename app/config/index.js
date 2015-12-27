7fd5ff035e1465cc31153861d9ae9ad221076373
Claus Klingberg <cjk@pobox.com>
2015-12-27 22:04:11 +0100

    Updates to KNX address-configuration and data.

--- app/config/index.js
+++ app/config/index.js
@@ -16,8 +16,8 @@ config.knx = {
   addresses: addresses,
   readableAddr: [
     '1/1/5', /* UG Keller-1-2 + Hobby1 via Tasterrückmeldung */
-    '1/1/6', /* UG Keller-2 Deckenleuchte via Präsenzmelder 1.1.42 Ausg. 0  */
-    '1/1/7', /* UG Keller-3 Deckenleuchte via Tasterrückmeldung */
+    '1/1/6', /* UG Keller-2 Deckenleuchte via Schaltaktor 1.1.2 Ausg. 14  */
+    '1/1/7', /* UG Keller-3 Deckenleuchte via Schaltaktor 1.1.2 Ausg. 5 */
     '1/2/1', /* EG Küche Deckenleuchten via Schaltaktor 1.1.2 Ausg. 3 */
     '1/2/6', /* EG WC Deckenleuchte via Schaltaktor 1.1.2 Ausg. 1 */
     '10/0/10', /* UG Fenster Keller-2 Kontakt via Binäreingang 1.1.39 Ausg. A-1  */
@@ -26,14 +26,15 @@ config.knx = {
     '1/2/12', /* EG Büro/Emma Hängeleuchte via Schaltaktor 1.1.45 Ausg. 5 */
     '1/2/2', /* EG/OG Treppenlicht LED via Schaltaktor 1.1.1 Ausg. 10 */
     '1/2/10', /* EG Wohnzimmer Erker Deckenleuchte via Schaltaktor 1.1.45 Ausg. 4 */
-    '1/3/4', /* DG Flur Wandleuchte via Schaltaktor 1.1.2 Ausg. 8 */
-    '1/3/12', /* DG Kind-1 / Schlafzimmer Deckenleuchte Nord via Dimmaktor 1.1.6 Ausg. 4 */
-    '11/2/0', /* DG Kind-2 / Daniel Steckdose Ost 1/5 via Schaltaktor 1.1.45 Ausg. 7 */
+    '1/3/4', /* OG Flur Wandleuchte via Schaltaktor 1.1.2 Ausg. 8 */
+    '1/3/12', /* OG Kind-1 / Schlafzimmer Deckenleuchte Nord via Dimmaktor 1.1.6 Ausg. 4 */
+    '11/2/0', /* OG Kind-2 / Daniel Steckdose Ost 1/5 via Schaltaktor 1.1.45 Ausg. 7 */
     '1/2/5', /* EG Flur Deckenleuchte via Schaltaktor 1.1.2 Ausg. 9 */
     '1/2/7', /* EG Küche Deckenleuchten Nord+Ost via Schaltaktor 1.1.5 Ausg. 1 */
     '1/3/2', /* OG Kind-3 Deckenleuchte via Schaltaktor 1.1.1 Ausg. 13 */
     '1/3/10', /* OG Bad Wand-/Waschtisch-Leuchten via Schaltaktor 1.1.1 Ausg. 7 */
-    '9/0/1', /* EG Wohnzimmer/Esszimmer/Küche Szene "Abendessen" via Schaltaktor 1.1.24 Ausg. 20 (Rückmeldeobjekt Taste 7) */
+    '9/0/1', /* EG Wohnzimmer/Esszimmer/Küche Szene "Abendessen" via Taster 1.1.24 Taste 7 (Rückmeldeobjekt Taste 7, Objekt A) */
+    '9/1/0', /* OG Kind-1 / Schlafzimmer Deckenleuchte Nord via Rückmeldeobjekt Taster 1.1.31 Taste 1 */
   ]
 };
 
