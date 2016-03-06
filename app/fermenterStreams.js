8d3a408762dce85f7eb1be5edaae0c73c41f1c06
Claus Klingberg <cjk@pobox.com>
2016-03-06 14:20:35 +0100

    Improve logging output.

--- app/fermenterStreams.js
+++ app/fermenterStreams.js
@@ -1,6 +1,8 @@
 import config from './config';
 import io from 'socket.io-client';
 import Kefir from 'kefir';
+import moment from 'moment';
+import {Map} from 'immutable';
 
 const {host, port} = config.fermenter;
 
@@ -10,13 +12,13 @@ export default function createFermenterStreams() {
 
   const fermenterStateStream = Kefir.stream(emitter => {
     socket.on('tempHumidity', (state) => {
-      console.log('Got Temp-Humidity-data:', state);
+      console.log('Got Fermenter-State:', JSON.stringify(new Map(state).update('createdAt', v => moment(v).format())));
       emitter.emit({'fermenterState': state});
     });
   });
 
   /* Request fermenter environmental data */
-  socket.emit('tempHumidity', {from: 'zircon'});
+  socket.emit('tempHumidity', {from: 'smart-home-backend'});
 
   return {
     fermenterState: fermenterStateStream
