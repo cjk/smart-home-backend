9576703ce1c86c40d97df465e9ffb04b5b63b4f8
Claus Klingberg <cjk@pobox.com>
2016-10-14 11:22:34 +0200

    Make bus-scanner faster.

--- app/lib/bus-scanner.js
+++ app/lib/bus-scanner.js
@@ -1,11 +1,11 @@
-import {readGroupAddr} from '../knx/performBusAction';
 import Kefir from 'kefir';
+import {readGroupAddr} from '../knx/performBusAction';
 
 export default function scanBusAddr(addresses) {
   /* NOTE: We're expecting a plain JS-array of address-IDs here right now - make
      sure you convert your immutable-List et.al. prior to calling the bus-scanner. */
-  const scanner = Kefir.sequentially(1000, addresses)
+  const scanner = Kefir.sequentially(500, addresses)
                        .map(addr => readGroupAddr(addr));
   scanner.onValue(() => {});
-  //scanner.log();
+  //   scanner.log();
 }
