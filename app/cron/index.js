9e47dda144305708c51d2faebc56bc35a64808f6
Claus Klingberg <cjk@pobox.com>
2016-11-01 16:51:49 +0100

    Refactor and properly stream task-results.

--- app/cron/index.js
+++ app/cron/index.js
@@ -1,15 +1,22 @@
 import {EventEmitter} from 'events';
-import R, {map, tap, assoc, merge, filter, compose, pipe, flatten, head, prop, props, propEq, isEmpty, not} from 'ramda';
+import R, {map, tap, pluck, assoc, merge, filter, compose, pipe, head, prop, props, propEq, isEmpty, not} from 'ramda';
 import K from 'kefir';
 import schedule from './schedule';
 import loadCrontab from './crontab';
 
-// const getWriteHandler = (_emitter) => (addr, cb) => {
-//   console.log(`Writing address ${addr}.`);
-//   setTimeout(() => _emitter.emit('foo'), 1000);
-//   console.log(`Address ${addr} written`);
-// };
+const scheduled = j => j.scheduled;
+const running = j => j.running;
 
+const scheduledJobIds = compose(
+  pluck('jobId'),
+  filter(running)
+);
+const runningJobIds = compose(
+  pluck('jobId'),
+  filter(scheduled)
+);
+
+/* Simulated fake async operation */
 const writeGroupAddr = (addr, cb) => {
   console.log(`Writing address ${JSON.stringify(addr)}...`);
   setTimeout(() => {
@@ -21,59 +28,65 @@ const writeGroupAddr = (addr, cb) => {
 const _crontab = loadCrontab();
 console.log(`Loaded crontab:\n <${JSON.stringify(_crontab)}>`);
 
-function init($busState) {
-  const $cron = K.withInterval(3000, (emitter) => {
+function init(busState$) {
+  const cron$ = K.withInterval(3000, (emitter) => {
     emitter.emit(_crontab);
   });
 
   const eventEmitter = new EventEmitter();
 
   /* TODO: Define action-result-stream that emits completed rules so we can set running=false in our state */
-  const actionResult$ = K.fromEvents(eventEmitter, 'actionFinished').toProperty(() => ({action: 'nothing'}));
+  const actionResult$ = K.fromEvents(eventEmitter, 'actionFinished').toProperty(() => {});
 
   const createAddrWriteStream = addr => K.fromNodeCallback((callback) => {
     writeGroupAddr(addr, callback);
   });
 
   /* Sideeffects routine */
-  const onValue = ({crontab}) => {
-    const scheduled = j => j.scheduled;
-    const hasScheduledJobs = !isEmpty(filter(scheduled)(crontab));
-    const onlyJobId = props(['jobId']);
+  const onValue = ({crontab, results}) => {
+    console.log(`[Results-onValue] ${JSON.stringify(results)}`);
 
-    if (not(hasScheduledJobs)) {
-      console.log('no jobs scheduled!');
-      return;
+    console.log(`[onValue] <${scheduledJobIds(crontab)}> jobs scheduled.`);
+    console.log(`[onValue] <${runningJobIds(crontab)}> jobs running.`);
+
+    if (!isEmpty(scheduledJobIds(crontab))) {
+      console.log(`Scheduled jobs: ${JSON.stringify(scheduledJobIds(crontab))}`);
     }
 
-    const scheduledJobIds = compose(flatten, map(onlyJobId), filter(scheduled));
-    console.log(`Running jobs ${JSON.stringify(scheduledJobIds(crontab))}`);
-
-    const task = pipe(
+    const firstScheduledTask = pipe(
       filter(scheduled),
       head,
       prop('tasks'),
       head,
-    )(crontab);
-    const result$ = createAddrWriteStream(task);
+    );
 
-    /* TODO: Need to trigger the action-results stream with done rule-id */
-    //     result$.flatMap(v => console.log(`got ${v}`));
-    result$.onValue((v) => { console.log(v); eventEmitter.emit('actionFinished', task); });
+    const taskStart = {status: 'started', startedAt: Date.now()};
+
+    if (!isEmpty(scheduledJobIds(crontab))) {
+      const task = firstScheduledTask(crontab);
+      const result$ = createAddrWriteStream(merge(task, taskStart));
+
+      /* TODO: Need to trigger the action-results stream with done rule-id */
+      //     result$.flatMap(v => console.log(`got ${v}`));
+      result$.onValue((taskState) => { console.log(`[result$] ${JSON.stringify(taskState)}`); eventEmitter.emit('actionFinished', taskState); });
+    }
   };
   /* END of sideeffects */
 
-  return K.combine([$cron, $busState], [actionResult$], (crontab, state, results) => {
+  return K.combine([cron$, actionResult$], [busState$], (crontab, results, state) => {
     /* PENDING: No logic here yet */
-    return {crontab, state, results};
+    return {crontab, results, state};
   }).scan((prev, cur) => {
     const {crontab, state, results} = cur;
 
+    console.log(`[Results-In-Stream] ${JSON.stringify(results)}`);
+
     const syncWithPrevJobs = map((j) => {
       const prevJob = R.find(R.propEq('jobId', j.jobId), prev.crontab);
-      if (!prevJob)
+      if (!prevJob) {
         /* TODO: Make sure returning nothing is OK here! */
         return j;
+      }
       return R.merge(j, R.pick(['running', 'scheduled'], prevJob));
     });
 
@@ -92,11 +105,17 @@ function init($busState) {
       scheduledJobs,
       R.adjust(setRunning, 0)
     );
-    console.log(`crontab: ${JSON.stringify(initiateJobs(schedCrontab))}`);
+
+    const newState = assoc('crontab', initiateJobs(schedCrontab), cur);
+
+    console.log(`<${scheduledJobIds(newState.crontab)}> jobs scheduled.`);
+    console.log(`<${runningJobIds(newState.crontab)}> jobs running.`);
+
+    console.log(`[finalSchedule]: ${JSON.stringify(newState.crontab)}`);
 
     //     console.log(`action-1: ${JSON.stringify(R.fromPairs([[1, [{'act':'off','id':1,'status':null,'startedAt':null,'endedAt':null,'target':'1/1/1'}, {'act':'off','id':1,'status':null,'startedAt':null,'endedAt':null,'target':'1/1/2'}]]]))}`);
 
-    return assoc('crontab', initiateJobs(schedCrontab), cur);
+    return newState;
   }).observe(onValue);
 }
 
