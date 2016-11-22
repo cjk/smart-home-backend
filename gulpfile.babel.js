8b49b265305e2c1b54b907c8dae386d3f74f10f3
Claus Klingberg <cjk@pobox.com>
2016-11-22 17:42:08 +0100

    Fix build.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,7 +16,7 @@ function build() {
       .pipe(preprocess())
       .pipe(babel({
         presets: ['latest'],
-        plugins: ['transform-flow-strip-types']
+        plugins: ['transform-flow-strip-types', 'transform-object-rest-spread']
       }))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest(DEST));
