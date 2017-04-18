7c72a587f75d267e1770473c4e0cbd96a9df50f5
Claus Klingberg <cjk@pobox.com>
2017-04-18 15:33:16 +0200

    Remove unused crontab-diff related code for now.
    We found other means to syncing crontab-changes working with deepstream-lists and their events (see previous commit(s)).

--- src/cron/index.js
+++ src/cron/index.js
@@ -7,12 +7,10 @@ import streamFromCloud from './streamFromCloud';
 /* How often to check crontab and schedule / dispatch jobs */
 const tickInterval = 1000;
 
-export default function init(
-  {
-    streams: { busState$ },
-    client,
-  }: { streams: { busState$: BusState }, client: Function }
-) {
+export default function init({
+  streams: { busState$ },
+  client,
+}: { streams: { busState$: BusState }, client: Function }) {
   const tick$ = K.interval(tickInterval, 1);
   const crontabFromCloud$ = streamFromCloud(client);
 
@@ -34,34 +32,10 @@ export default function init(
       [busState$],
       (crontab, taskEvents, state) => ({ crontab, taskEvents, state })
     )
-    /* Jobs and tasks get synced (from last tick), scheduled and (indirectly) run from here: */
-     .scan(scheduleTick)
-    // .scan((p, c) => {
-    //   const { crontab: a } = p;
-    //   const { crontab: b } = c;
-       // console.log(`< ${JSON.stringify(a)}`);
-       // console.log(`> ${JSON.stringify(b)}`);
-    //        return p;
-
-       // var inJ = (lst, id) => R.any(R.propEq('jobId', id))(lst);
-       // var inJx = R.curry(inJ)
-
-       // var inJ1 = inJx(v1)
-       // var inJ2 = inJx(v2)
-
-       // var isNew = (id) => (R.not(inJ1(id)) && inJ2(id));
-       // var isRemoved = (id) => (inJ1(id) && R.not(inJ2(id)));
-       // var isModified = (id) => (inJ1(id) && inJ2(id));
-
-       // R.compose(R.pluck('jobId'), R.symmetricDifference)(v1, v2)
-       // [ 2, 3, 3, 4, 5 ]
-       // var jobIds = [ 2, 3, 4, 5 ]
-
-       // R.reduceBy((acc, jobId) => acc.concat(jobId), [], (jobId) => (isNew(jobId) ? 'isNew' : isRemoved(jobId) ? 'isRemoved' : 'isModified'))
-    //      })
-
-    /* Subscribe to cron-stream and return a Subscription object for handling unsubscribe - see
+      /* Jobs and tasks get synced (from last tick), scheduled and (indirectly) run from here: */
+      .scan(scheduleTick)
+      /* Subscribe to cron-stream and return a Subscription object for handling unsubscribe - see
        http://rpominov.github.io/kefir/#observe */
-     .observe(processTaskEvents())
+      .observe(processTaskEvents())
   );
 }
