b899bcd1672615d50d4591169dea85685e4e34d5
Claus Klingberg <cjk@pobox.com>
2017-08-17 11:12:15 +0200

    Catch exception / crash when writing to knx-bus with bogus address-formats

--- src/server/busServer/handleBusWrites.js
+++ src/server/busServer/handleBusWrites.js
@@ -6,22 +6,32 @@ import K from 'kefir';
 import Address from '../../knx/address';
 import { writeGroupAddr } from '../../knx/performBusAction';
 
-const createBusWriteEventSubStream = client => K.stream(emitter => {
-  const onBusWrite = addr => {
-    emitter.emit(addr);
-  };
-  client.event.subscribe('knx/writeGroupAddr', onBusWrite);
-  return () => {
-    client.event.unsubscribe('knx/writeGroupAddr', onBusWrite);
-  };
-});
+const createBusWriteEventSubStream = client =>
+  K.stream(emitter => {
+    const onBusWrite = addr => {
+      emitter.emit(addr);
+    };
+    client.event.subscribe('knx/writeGroupAddr', onBusWrite);
+    return () => {
+      client.event.unsubscribe('knx/writeGroupAddr', onBusWrite);
+    };
+  });
 
 function writeAddressToBus(addr) {
   console.log(
-    `[handleBusWrites] About to perform address-write on request for address: ${JSON.stringify(addr)}`
+    `[handleBusWrites] About to perform address-write on request for address: ${JSON.stringify(
+      addr
+    )}`
   );
   if (addr.id) {
-    writeGroupAddr(new Address(addr));
+    // Sending a new value for an address in a specific format to the knx-bus may fail. Catch and print errors but don't let it crash
+    try {
+      writeGroupAddr(new Address(addr));
+    } catch (e) {
+      console.error(
+        `Failed to write to KNX-bus for address ${addr.id}. Check the address-type and format.`
+      );
+    }
   } else {
     console.error(
       '[handleBusWrites] ERROR - Illegal KNX-address received for bus-writing - will not perform any bus action!'
