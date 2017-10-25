9f015c5826963958821c2fe8dc44184d48e42ec8
Claus Klingberg <cjk@pobox.com>
2017-10-25 20:18:22 +0200

    Fix named type.

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,12 @@
 /* @flow */
 
 import logger from 'debug';
-import type { ServerProps } from '../types';
+import type { ServerState } from '../types';
 import busServer from './busServer';
 
 const debug = logger('smt:server');
 
-function publish(props: ServerProps) {
+function publish(props: ServerState) {
   busServer(props);
   debug('==> Server started');
 }
