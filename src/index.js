ecf58af588a4031fc2efb5136ed4d57cda1395ca
Claus Klingberg <cjk@pobox.com>
2018-04-24 19:46:10 +0200

    Remove unneeded piping module and upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -2,13 +2,4 @@
 
 // Load some environment-variables from local files - see ~/.env*
 require('dotenv').config();
-
-function enablePiping(env) {
-  if (env !== 'production') {
-    if (!require('piping')({ hook: true })) {
-      return;
-    }
-  }
-  require('./main');
-}
-enablePiping(process.env.NODE_ENV);
+require('./main');
